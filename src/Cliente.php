9d22fdbe70193cac6a5aaf5acf02be9de1c3b0dc
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-04-17 09:09:47 -0300

    Create Cliente.php

---
+++ src/Cliente.php
@@ -0,0 +1,52 @@
+<?php
+class Cliente
+{
+    private string $nome;
+    private string $email;
+    private Situacao $situacao;
+
+    /* Definindo a situação INATIVO como valor padrão para o parâmetro situação no construtor. */
+    public function __construct(
+        string $nome, string $email, Situacao $situacao = Situacao::INATIVO
+        )
+    {
+        $this->setNome($nome);
+        $this->setEmail($email);
+        $this->setSituacao($situacao);
+    }
+
+    private function setSituacao(Situacao $situacao):void {
+        $this->situacao = $situacao;
+    }
+
+    private function getSituacao(Situacao $situacao):void {
+        $this->situacao = $situacao;
+    }
+
+    private function setNome(string $nome): void 
+    {
+        $this->nome = $nome;
+    }
+
+
+    private function setEmail(string $email): void 
+    {
+        if( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
+            throw new InvalidArgumentException("E-mail inválido!");
+        }
+
+        $this->email = $email;
+    }
+
+    public function getNome(): string 
+    {
+        return $this->nome;
+    }
+
+
+    public function getEmail(): string 
+    {
+        return $this->email;
+    }
+    
+}
